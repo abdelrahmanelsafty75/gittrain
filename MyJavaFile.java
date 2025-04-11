b0a30f9e8eeae0399b502a57dfc636178168ea32
ahmedNewiry <ahmedabdalfttah@outlook.com>
2025-04-11 03:34:16 +0200

    Ahmed's updates

--- MyJavaFile.java
+++ MyJavaFile.java
@@ -2,5 +2,6 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Hello With Java");
      System.out.println("let's practice :)");
+     System.out.println('This is changes have been made by Ahmed');
   }
 }
